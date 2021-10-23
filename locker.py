42042355b1ebee15e78308b9d6e2f432ba9e51b1
emelda <YOUR EMAIL ADDRESS>
2021-10-23 15:12:41 +0300

    Add a test to check if the user object or instances are saved.

--- locker.py
+++ locker.py
@@ -2,6 +2,7 @@ class User:
     '''
     To initialize the User instances
     '''
+    locker_list = [] #empty locker list
     def __init__(self,first_name,last_name,username):
         '''
         __init__ method that defines the objects' properties
@@ -10,3 +11,8 @@ class User:
         self.last_name = last_name
         self.username = username
     
+    def save_user(self):
+        '''
+        method to save user objects into the locker_list
+        '''
+        User.locker_list.append(self)
--- locker_test.py
+++ locker_test.py
@@ -1,5 +1,5 @@
 import unittest
-from users import User
+from locker import User
 
 class TestUser(unittest.TestCase):
     '''
@@ -10,3 +10,22 @@ class TestUser(unittest.TestCase):
         Set up method to run before each test
         '''
         self.new_user= User("Emelda", "Perez", "acetips")
+
+    def test_init(self):
+        '''
+        test_init method to test if object is properly initialized
+        '''
+        self.assertEqual(self.new_user.first_name,"Emelda")
+        self.assertEqual(self.new_user.last_name,"Perez")
+        self.assertEqual(self.new_user.username,"acetips")
+
+    def test_save_user(self):
+        '''
+        a test to ckeck if the user object is saved
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.locker_list),1)
+
+
+if __name__ == '__main__':
+    unittest.main()
