37d5b4ee935ed3cb7bfd79b174a303476e25c433
emelda <YOUR EMAIL ADDRESS>
2021-10-23 13:45:15 +0300

    Create a test file.

--- locker.py
+++ locker.py
@@ -1,1 +1,12 @@
-class User
+class User:
+    '''
+    To initialize the User instances
+    '''
+    def __init__(self,first_name,last_name,username):
+        '''
+        __init__ method that defines the objects' properties
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+    
