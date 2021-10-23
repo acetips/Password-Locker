02b094b246f07b8ebe528495515d36880393c2b0
emelda <YOUR EMAIL ADDRESS>
2021-10-23 13:55:58 +0300

    Add a new user object.

--- locker_test.py
+++ locker_test.py
@@ -1,2 +1,12 @@
 import unittest
-from user import User
+from users import User
+
+class TestUser(unittest.TestCase):
+    '''
+    class to define test cases for user class behavior
+    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test
+        '''
+        self.new_user= User("Emelda", "Perez", "acetips")
