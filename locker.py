6a886d6565641ea3e2176d05d987f7da3d14b544
emelda <YOUR EMAIL ADDRESS>
2021-10-24 13:59:18 +0300

    create the run.py file.

--- locker.py
+++ locker.py
@@ -1,6 +1,6 @@
 class User:
     '''
-    To initialize the User instances
+    class for initializing the User instances
     '''
     locker_list = [] #empty locker list
 
@@ -19,8 +19,57 @@ class User:
         '''
         User.locker_list.append(self)
 
+    @classmethod
+    def user_exists(cls, username):
+        for user in cls.locker_list:
+            if user.username == username:
+                return True
+        return False
+
 class Credentials:
+    '''
+    class for initializing the user's credentials
+    '''
+    credentials_list =[]#empty list for user details
 
+    def __init__(self,website,website_username,website_password):
+        '''
+        method to define object's properties
+        '''
+        self.website = website
+        self.website_username = website_username
+        self.website_password = website_password
+
+    def save_credentials(self):
+        '''
+        method to save new credentials of the user
+        '''
+        Credentials.credentials_list.append(self)
+
+    def delete_credentials(self):
+        '''
+        a method for deleting the saved credentials of the user
+        '''
+        Credentials.credentials_list.remove(self)
+
+    @classmethod
+    def find_by_website(cls, website):
+        for credential in cls.credentials_list:
+            if credential.website == website:
+                return credential
     
+    def test_display_credentials(cls):
+        return cls.credentials_list
 
+   
+
+    # @classmethod
+    # def verify_credentials(cls, name, lock):
+    #     '''
+    #     Method that checks if the username and password are correct
+    #     '''
+    #     for user in cls.credentials_list:
+    #         if user.user_name == name and user.password == lock:
+    #             return user
+    #     return 0
     
