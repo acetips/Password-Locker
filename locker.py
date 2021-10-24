64602f4efcd22e5c3700c6919d5bd9fd7895c9cc
emelda <YOUR EMAIL ADDRESS>
2021-10-24 11:58:46 +0300

    Create a test class to define test cases for the credentials class behaviours.

--- locker.py
+++ locker.py
@@ -3,10 +3,12 @@ class User:
     To initialize the User instances
     '''
     locker_list = [] #empty locker list
+
     def __init__(self,first_name,last_name,username):
         '''
         __init__ method that defines the objects' properties
         '''
+        
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
@@ -16,3 +18,9 @@ class User:
         method to save user objects into the locker_list
         '''
         User.locker_list.append(self)
+
+class Credentials:
+
+    
+
+    
--- locker_test.py
+++ locker_test.py
@@ -19,12 +19,28 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.last_name,"Perez")
         self.assertEqual(self.new_user.username,"acetips")
 
+    def tearDown(self):
+        '''
+        method that clears the locker_list after running each test
+        '''
+        User.locker_list=[]
+
     def test_save_user(self):
         '''
-        a test to ckeck if the user object is saved
+        a test to check if the user object is saved
         '''
         self.new_user.save_user()
         self.assertEqual(len(User.locker_list),1)
+
+class TestCredentials(unittest.TestCase):
+    '''
+    Test class to define test cases for behaviours in the credentials class
+    '''
+    def test_multiple_credentials(self):
+        '''
+        a test to save user credentials for multiple websites or applications
+        '''
+        self.new
 
 
 if __name__ == '__main__':
