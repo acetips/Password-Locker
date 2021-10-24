83ae6a90132cb48f0e63f09b6b7f730476dcbc1f
emelda <YOUR EMAIL ADDRESS>
2021-10-24 16:31:21 +0300

    Add a UI section for creating an account and logging in to the account.

--- run.py
+++ run.py
@@ -45,3 +45,56 @@ def show_all_credentials():
     A function to display all the user's credentials
     '''
     return Credentials.display_credentials()
+
+def main():
+    print ("Welcome to password locker. What's your name?")
+    name = input()
+    print(f"Greetings {name}!, Would you like to create an account or login to your existing account?")
+    print('\n')
+    while True:
+        print("To create an account, use 'ca';To login to your account, use 'la';To exit the program, use 'ex'")
+        user_input = input().lower()
+        if user_input == 'ca':
+            print ("Create an account")
+            print("*"*6)
+            print("First Name:")
+            first_name = input()
+            print("Last Name:")
+            last_name = input()
+            print("Preferred username:")
+            username = input()
+            print("Preferred password:")
+            mypassword = input()
+        elif user_input == 'la':
+            print ("Login to your account")
+            print("*"*6)
+            print("Enter your username:")
+            username = input()
+            print("Enter your password")
+            mypassword = input()
+
+            save_user(create_passwordlocker_account(first_name,last_name,username,password))
+            print('\n')
+            print("Congratulations!Your account has been created successfully. Here are your details:")
+            print(f"Account details {first_name} {last_name} {username} {password}")
+            print('\n')
+
+        elif user_input == 'la':
+            print("Account login")
+            print("*"*6)
+            print("Username:")
+            username = input()
+            print("Enter your password:")
+            mypassword = input()
+            print('\n')
+        
+        elif user_input == 'ex':
+            print("We are sad to see you go:(.")
+            break
+        
+        else:
+            print("Invalid user entry. kindly re-enter a valid command")
+            print("\n")
+
+if __name__ =='__main__':
+    main()
